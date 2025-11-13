7d57d6a220ef1c916e0caed42a0b6848c1039fae
Samir Ramić <ramic.samirr@gmail.com>
2025-11-13 21:28:57 +0100

    - Model corrections

--- bbquotes/Models/Death.swift
+++ bbquotes/Models/Death.swift
@@ -5,9 +5,11 @@
 //  Created by Samir Ramic on 13.11.25.
 //
 
+import Foundation
+
 struct Death: Codable {
     let character: String
-    let image: String
+    let image: URL
     let details: String
     let lastWords: String
 }
